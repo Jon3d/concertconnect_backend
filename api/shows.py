477bad21756991b42d4e6d043f4890da84b6a7ca
System Administrator <jon.huisingh@gmail.com>
2014-06-07 17:47:00 -0700

    added apps

---
+++ api/shows.py
@@ -0,0 +1,18 @@
+'''
+Created on Jun 7, 2014
+
+@author: jhuisingh
+'''
+
+class Shows(object):
+    '''
+    classdocs
+    '''
+    pass
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        pass
