b5e60189dfa6dc14a6e40d6f642ec8e7912dc80a
Jonathan Huisingh <jon.huisingh@gmail.com>
2014-06-10 10:12:06 -0700

    fix redirect

--- concertconnect_backend/urls.py
+++ concertconnect_backend/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'concertconnect_backend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^', RedirectView.as_view(url='http://www.google.com')),
+    url(r'^/', RedirectView.as_view(url='http://www.google.com')),
     
     
     url(r'^admin/', include(admin.site.urls)),
