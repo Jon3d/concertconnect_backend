7f3b12dfd6a1163979b536140fc96f371e68b09a
Jonathan Huisingh <jon.huisingh@gmail.com>
2014-06-10 19:23:12 -0700

    fixed typo in manage.py

--- concertconnect_backend/wsgi.py
+++ concertconnect_backend/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concertconnect_backend.settings_base")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "concertconnect_backend.settings.settings_prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
