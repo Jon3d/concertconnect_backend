8da176dd1b2de81874a6badffa81c9038ab86d5c
Jonathan Huisingh <jon.huisingh@gmail.com>
2014-06-10 19:35:01 -0700

    apparently heroku doesnt like logs

--- concertconnect_backend/settings/settings_prod.py
+++ concertconnect_backend/settings/settings_prod.py
@@ -21,35 +21,3 @@ LAST_FM_NETWORK = pylast.LastFMNetwork(api_key = LAST_FM_API_KEY,
                                api_secret = LAST_FM_SECRET_KEY, 
                                username = LAST_FM_USERNAME, 
                                password_hash = LAST_FM_PASSWORD_HASH)
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'verbose': {
-            'format' : "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-            'datefmt' : "%d/%b/%Y %H:%M:%S"
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': 'logs/cc_backend.log',
-            'formatter': 'verbose'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers':['file'],
-            'propagate': True,
-            'level':'DEBUG',
-        },
-        'MYAPP': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-        },
-    }
-}
