622839f30bb1e9f7df26ad6001aff366f91443f7
Jonathan Huisingh <jon.huisingh@gmail.com>
2014-06-18 14:27:11 -0700

    fixed urls

--- concertconnect_backend/urls.py
+++ concertconnect_backend/urls.py
@@ -17,7 +17,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'concertconnect_backend.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'^/', RedirectView.as_view(url='http://www.google.com')),
+    url(r'^', RedirectView.as_view(url='http://www.google.com')),
     
     url(r'^api-rest-framework/', include(router.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
